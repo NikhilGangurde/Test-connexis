0fe7e18b50890f33154a48a3f8cfdc3be130e3b4
Nikhil <nikhil@mauryacravings.com>
2019-01-22 16:46:18 +0530

    Api Interaction
    Both api is called and response is assign to respective variables for use

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,6 +3,35 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$http','$scope' , function($http,$scope)  //controller for viewctrl page
+{ 
+
+    $scope.images = [];
+
+
+    $http({
+        method: 'GET',
+        url: 'http://demo8499921.mockable.io/angular'           // requesting api using $http get method for data
+     }).then(function (response){
+        console.log("Data : "+JSON.stringify(response));        //Getting data as Response on success
+        $scope.data = response.titles;
+     },function (error){
+        console.log("Data : "+JSON.stringify(error));           //log If any error
+     });
+
+     $http({
+        method: 'GET',
+        url: 'https://picsum.photos/list'                       // requesting api using $http get method for images
+     }).then(function (response){
+        console.log("Data : "+JSON.stringify(response));        //Getting Images as Response on success
+        for(var i=0;i<5;i++)
+        {
+            $scope.images.push(response.data[i]);               //Getting first 5 Images from Response in new array of Images
+        }
+        console.log("Data : "+JSON.stringify($scope.images));
+     },function (error){
+        console.log("Data : "+JSON.stringify(error));           //log If any error
+     });
+  
   
 }]);
