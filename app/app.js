a1e3d8238cb6568ce8f1d7a26a53ce84aa851983
Nikhil <nikhil@mauryacravings.com>
2019-01-23 12:51:25 +0530

    grid css change for exact look

--- app/app.js
+++ app/app.js
@@ -3,22 +3,16 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
-  // 'ngMaterial',
+  'ngMaterial',
   'myApp.view1',
-  'myApp.view2',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider','$qProvider', function($locationProvider, $routeProvider, $qProvider) {
   $locationProvider.hashPrefix('!');
 
-
     $routeProvider.when('/view1', {
       templateUrl: 'view1/view1.html',
       controller: 'View1Controller'
-    });
-    $routeProvider.when('/view2', {
-      templateUrl: 'view2/view2.html',
-      controller: 'View2Controller'
     });
 
   $routeProvider.otherwise({redirectTo: '/view1'});
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,10 @@
 
 angular.module('myApp.view1', ['ngRoute','ngMaterial'])
 
-
 .controller('View1Controller', ['$http','$scope' , function($http,$scope)  //controller for viewctrl page
 { 
-
     $scope.images = [];
     $scope.imagesfinal = [];
-
 
     $http({
         method: 'GET',
@@ -16,26 +13,21 @@ angular.module('myApp.view1', ['ngRoute','ngMaterial'])
      }).then(function (response){
         $scope.data = response.data.titles;
      },function (error){
-        console.log("Data : "+JSON.stringify(error));           //log If any error
+        console.log("Data : "+JSON.stringify(error));           // log for Error if any
      });
 
      $http({
         method: 'GET',
         url: 'https://picsum.photos/list'                       // requesting api using $http get method for images
      }).then(function (response){
-        for(var i=6;i<11;i++)
+        for(var i=6;i<15;i++)
         {
             var s=response.data[i].post_url
-            var final = s.substr(s.lastIndexOf('/') + 1);
-       
-            var photo = new UnsplashPhoto();
-       
-           photo.find(final).fetch();
-       
-           $scope.images.push(photo);
-
+            var final = s.substr(s.lastIndexOf('/') + 1);       //  Sub string function for gettin id from image url
+            var photo = new UnsplashPhoto();                    // Unsplash object creation
+            photo.find(final).fetch();                           //  unsplash find function call using photo id 
+            $scope.images.push(photo);                           // new array of url's of images
         }
-        
      },function (error){
         console.log("Data : "+JSON.stringify(error));           //log If any error
      });
